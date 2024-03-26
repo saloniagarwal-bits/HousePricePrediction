70921e7e0bbd15c6ee857931b5f3d75533a90e04
[Saloni Agarwal] <[saloni.bitspilani@gmail.com]>
2024-03-26 14:27:24 +0530

    Modified app and html file

--- app.py
+++ app.py
@@ -24,5 +24,14 @@ def predict_api():
     return jsonify(output[0])
     #since output is 2D array, so take 0 index
 
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=scaler.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=model.predict(final_input)[0]
+    return render_template('home.html',prediction_text='Predicted house price is {}'.format(output))
+
+
 if __name__=="__main__":
     app.run(debug=True)
